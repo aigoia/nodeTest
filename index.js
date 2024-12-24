a6fbb79d74b4e5a267c6c1374f9c08420b55561b
aigoia <oidoicompany@gmail.com>
2024-12-25 07:29:04 +0900

    set  init page

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const raylib = require('raylib')
 const setting = require('./setting')
 const print = require('./print')
 
-const initGame = () => {
+const countdown = async (start = init_count) => {
+    for (let i = start; i > 0; i--) {
+        raylib.BeginDrawing()
+        raylib.ClearBackground(setting.Snow)
+    
+        raylib.DrawText(`${i}`, (setting.ScreenWidth - setting.ScoreMargin) / 2, setting.ScreenHeight / 2 - setting.ScoreSize, setting.ScoreSize, setting.Mint)
+
+        raylib.EndDrawing()
+        await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+}
+
+const initGame = async() => {
     print("hello pong!")
     raylib.InitWindow(setting.ScreenWidth, setting.ScreenHeight, setting.GameName)
     raylib.SetTargetFPS(setting.Frame)
+
+    await countdown(setting.InitCount)
 }
 
 const updateGame = () => {
@@ -19,10 +33,12 @@ const drawGame = () => {
     raylib.EndDrawing()    
 }
 
-
-initGame()
-while (!raylib.WindowShouldClose()) {
-    updateGame()
-    drawGame()
+const main = async () => {    
+    await initGame()
+    while (!raylib.WindowShouldClose()) {
+        updateGame()
+        drawGame()
+    }
+    raylib.CloseWindow()
 }
-raylib.CloseWindow()
+main()
